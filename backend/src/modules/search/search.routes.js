6d4ac8d590dc7bbfbd299311a07208252281929f
Mariam <ha.rin00@mail.ru>
2025-06-27 12:12:52 +0300

    Delete backend/src directory

--- backend/src/modules/search/search.routes.js
+++
@@ -1,9 +0,0 @@
-// modules/search/search.routes.js
-import express from "express";
-import { searchMealOrChef } from "./search.controller.js";
-
-const router = express.Router();
-
-router.get("/", searchMealOrChef); // /api/search?keyword=كلمة
-
-export default router;
